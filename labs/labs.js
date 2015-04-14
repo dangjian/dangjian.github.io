db25c0eac276a77f70adb6f32f2783fe686ec1bc
dangjian <hunterdang@gmail.com>
2015-04-14 19:12:43 +0800

    presentation

--- labs/labs.js
+++ labs/labs.js
@@ -21,6 +21,20 @@ app.config(['$routeProvider', function ($routeProvider) {
         templateUrl: function(params){ return "JavaScriptClass/" + params.classId + ".html"}
     });
 
+    $routeProvider.when("/pathmenu", {
+        title: '仿Path菜单',
+        templateUrl: 'pathmenu/index.htm',
+        action: function() {
+            $('#menu-container').pathmenu({ base: '#base_button', direction: 'topright', width: 150 });
+            $('#direction_list').bind('change', function (e) {
+                var direction = e.target.value;
+                var width = 150;
+                if (direction == 'right' || direction == 'left') width = 400;
+                $('#menu-container').pathmenu({ base: '#base_button', direction: direction, width: width });
+            })
+        }
+    });
+
     $routeProvider.otherwise({ redirectTo: "/home" });
 }]);
 
@@ -34,3 +48,40 @@ app.run(['$rootScope', function($rootScope){
         $(".collapse").collapse();
     };
 }]);
+
+app.directive('head', ['$rootScope','$compile',
+    function($rootScope, $compile){
+        return {
+            restrict: 'E',
+            link: function(scope, elem){
+                var html = '<link rel="stylesheet" ng-repeat="(routeCtrl, cssUrl) in routeStyles" ng-href="{{cssUrl}}" >';
+                elem.append($compile(html)(scope));
+
+                scope.routeStyles = {};
+
+                $rootScope.$on('$routeChangeStart', function (e, next, current) {
+
+                    if(current && current.$$route && current.$$route.css){
+                        if(!Array.isArray(current.$$route.css)){
+                            current.$$route.css = [current.$$route.css];
+                        }
+                        angular.forEach(current.$$route.css, function(sheet){
+                            delete scope.routeStyles[sheet];
+                        });
+                    }
+
+                    if(next && next.$$route && next.$$route.css){
+                        if(!Array.isArray(next.$$route.css)){
+                            next.$$route.css = [next.$$route.css];
+                        }
+                        angular.forEach(next.$$route.css, function(sheet){
+                            scope.routeStyles[sheet] = sheet;
+                        });
+                    }
+
+                });
+
+            }
+        };
+    }
+]);
